65326cc2830c375c0c991edc4f3598599f927f05
rohidasgowda@gmail.com <rohidasgowda@gmail.com>
2023-07-05 22:11:35 +0530

    feat: add register on server side

--- server/index.js
+++ server/index.js
@@ -1,1 +1,30 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const Users = require('./models/user.model')
 
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.post('/api/register', async (req, res) => {
+    try {
+        await Users.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({ status: true })
+    } catch (error) {
+        res.json({ status: false })
+    }
+})
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Server started on ${process.env.SERVER_PORT}`)
+})
